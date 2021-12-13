0c0248894ac9173675e1b8f94b1b340e36ef3030
HTrieu <lehaitrieu48@gmail.com>
2021-12-13 19:51:27 +0700

    End Commit

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Search.scss';
+//import './Search.scss';
 
 function Search(){
     return <div className="search">
         <form className='search-form'>
             <input type="text" placeholder=" "/>
-            <button type='reset'></button> 
+            <button type='reset'></button>
         </form>
     </div>
 }
