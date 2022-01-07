ed6aab457d101e5c85a6fe765c8c89d6300f82e1
HTrieu <lehaitrieu48@gmail.com>
2022-01-08 03:37:57 +0700

    1234

---
+++ src/components/BookRoom/bookRoomList.js
@@ -0,0 +1,51 @@
+import React, {useContext, useEffect, useState } from 'react';
+import Axios from 'axios';
+
+import UserContext from '../../context/UserContext';
+import Room from './bookRoom';
+ 
+ 
+function RoomList({payBill}) {
+    const [rooms, setRooms] = useState([]);
+    const {user} = useContext(UserContext)
+
+    useEffect(() =>{
+		if(!user) setRooms([]);
+		else getRooms();
+	},[user]);
+
+    async function getRooms() {
+		const roomData = await Axios.get("http://localhost:5000/bookRoom/");
+		console.log(roomData);
+		let sortedEmployees  = [...roomData.data];
+		sortedEmployees = sortedEmployees.filter(a => a.stateGiveMoney===payBill);
+		console.log(roomData);
+		setRooms(sortedEmployees);	
+	}
+
+    function renderEmployees() {
+		
+			const sortedEmployees =  rooms.map((room,i) =>{
+				return <Room key={i} room ={room}
+				getRooms={getRooms}
+				/>
+			})
+			return sortedEmployees;
+	}
+   
+   return <>
+    {user === null&&(
+			<h2>Hãy đăng nhập để thao tác trên hệ thống</h2>
+	  )}
+      {
+          user!==null && (
+              <>
+              {rooms.length > 0 ? renderEmployees() : (
+					<h3>Bạn chưa thuê phòng. Hãy đặt phòng yêu thích của bạn.</h3>
+				)}</>
+          )
+      }
+    </>;
+};
+
+export default RoomList;
